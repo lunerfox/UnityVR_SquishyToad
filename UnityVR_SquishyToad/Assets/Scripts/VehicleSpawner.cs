a5695906f87433e0a5c055a9c851a74e883a976c
Szeto <Lunerfox@gmail.com>
2016-06-14 15:08:14 -0700

    Modified the vehicle spawner code to spawn vehicles attached script to road pre-fab to spawn cars.

--- UnityVR_SquishyToad/Assets/Scripts/VehicleSpawner.cs
+++ UnityVR_SquishyToad/Assets/Scripts/VehicleSpawner.cs
@@ -4,10 +4,18 @@ using System.Collections;
 public class VehicleSpawner : MonoBehaviour {
 
 	public GameObject[] vehiclePrefabs;
+	public float heightOffset;
+	public float startOffset;
 
 	// Use this for initialization
 	void Start () {
-		Instantiate(vehiclePrefabs[0], transform.position, transform.rotation);
+		Vector3 position = transform.position;
+		position += heightOffset * Vector3.up;
+		position += startOffset * Vector3.right;
+		int CarType = Random.Range(0, vehiclePrefabs.Length);
+		GameObject vehicleObject = Instantiate(vehiclePrefabs[CarType]);
+		vehicleObject.transform.position = position;
+		vehicleObject.transform.parent = transform;
 	}
 	
 	// Update is called once per frame
