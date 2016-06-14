2adbddc3bcb4ae83877ee45e5ab8b02ef2ddf411
Szeto <Lunerfox@gmail.com>
2016-06-14 14:48:55 -0700

    Worked up to Lecture 76. Turns out I wasn't saving the scenes so I had to redo things 2 times after each crash from Unity. Figured it out now.

---
+++ UnityVR_SquishyToad/Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+	public GameObject[] vehiclePrefabs;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(vehiclePrefabs[0], transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
