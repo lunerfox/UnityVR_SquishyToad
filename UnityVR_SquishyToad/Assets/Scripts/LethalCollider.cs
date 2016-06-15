e04ceae1622de2eb41e4d10801ce7c7b87abdfdf
Szeto <Lunerfox@gmail.com>
2016-06-15 09:15:05 -0700

    Added Lethal Collider: Kills frog if an object with the script hits it. Updated PreFabs Car and Van with the lethal collider

---
+++ UnityVR_SquishyToad/Assets/Scripts/LethalCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Player player = collision.gameObject.GetComponent<Player>();
+		if(player)
+		{
+			GameState gameState = FindObjectOfType<GameState>();
+			gameState.IsGameOver = true;
+		}
+	}
+}
