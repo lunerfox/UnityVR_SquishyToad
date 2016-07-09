34e5cda2422eebc96cd21b4bae7cbbd94741bab3
lunerfox <lunerfox@gmail.com>
2016-07-09 13:35:57 -0700

    Started cloud implementation (Not finished) Added more scenery.

---
+++ UnityVR_SquishyToad/Assets/Scripts/Spawners/CloudSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Spawns clouds into the scene
+public class CloudSpawner : MonoBehaviour {
+
+    public GameObject[] cloudPrefabs;   //Array of clouds available
+    public float heightOffset;
+    public float startOffset;
+    public float minDistance;           //Minimum Distance between clouds
+    public float maxDistance;           //Maximum Distance between clouds
+    public float maxRenderDistance;     //Distance away from the player to render clouds at (On both ends?)
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
