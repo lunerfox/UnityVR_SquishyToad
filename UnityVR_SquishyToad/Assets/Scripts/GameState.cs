0c7a9962ade39354c996976c5dec55a21c0b2f42
lunerfox <lunerfox@gmail.com>
2016-07-10 16:16:12 -0700

    Organized scripts for objects into its own folder Fixed a bug with the Canvas UI Prefab where the high score script was still in the prefab. Added rotation to all of the wheels - Vehicles now look like they're actually being driven by the wheels, not gliding on ice! Wheels now turn based on the speed of the vehicle; Fast cars have tires that spin faster.

--- UnityVR_SquishyToad/Assets/Scripts/GameState.cs
+++ UnityVR_SquishyToad/Assets/Scripts/GameState.cs
@@ -3,12 +3,24 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour {
-
+    private float _highscore;
+    
 	public bool IsGameOver   { get; set;}   //Game Over state is HIGH when the player has lost but has not restarted.
     public bool IsIdle       { get; set;}   //Idle state is HIGH when the player has restarted but not ready
+    
     //High Score is only recorded if the updated score is better then the high score.
-	public float HighScore    { get { return HighScore; } set { if (value > HighScore) HighScore = value; } }
-	public int CurLevel      {get; set;}
+	public float HighScore    {
+        get { return Mathf.Floor(_highscore); }
+        set {
+                print("A new High Score! Congrats!");
+                if (Mathf.Floor(value) > _highscore) _highscore = value;
+                IsHighScore = true;
+            }
+    }
+
+    public bool IsHighScore { get; set;}   //A High Score has been achieved this round
+
+    public int CurLevel      {get; set;}
 
     //TODO: Separate power-ups into its own separate handler
     public bool PwrUpJump { get; set;}
@@ -24,6 +36,8 @@ public class GameState : MonoBehaviour {
         PwrUpJumpCooldown = 10.0f;
         IsGameOver = false;
         IsIdle = true;
+        _highscore = 0;
+        IsHighScore = false;
     }
     
     void Update () {
---
+++ UnityVR_SquishyToad/Assets/Scripts/Misc/turnWheel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Rotates the wheels of a vehicle. Put script onto the wheel itself.
+
+public class turnWheel : MonoBehaviour {
+
+    //GameObject carModel;
+    Vehicle vehicle;
+    public float wheelRotationMod;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get the speed of the parent vehicle
+        vehicle = transform.parent.parent.gameObject.GetComponent<Vehicle>();
+        float vehicleSpeed = vehicle.speed;
+        bool vehicleDirection = vehicle.direction;
+        if (vehicleDirection)   transform.Rotate(0, 0, -vehicleSpeed * wheelRotationMod * 360 * Time.deltaTime);
+        else                    transform.Rotate(0, 0, vehicleSpeed * wheelRotationMod * 360 * Time.deltaTime);
+    }
+}
